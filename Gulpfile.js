270ec214886f31737887151b32526664edcd2660
Bastian Bloessl <mail@bastibl.net>
2018-05-19 14:23:07 +0100

    news

--- Gulpfile.js
+++ Gulpfile.js
@@ -7,12 +7,15 @@ var data = require('gulp-data');
 var marked = require('marked');
 var nunjucksRender = require('gulp-nunjucks-render');
 var del = require('del');
-
+var frontMatter = require('gulp-front-matter');
 var sass = require('gulp-sass');
 var sourcemaps   = require('gulp-sourcemaps');
 var cssmin = require('gulp-cssmin');
 var autoprefixer = require('gulp-autoprefixer');
 var dateFilter = require('nunjucks-date-filter');
+var through = require('through2');
+var fs = require('fs');
+var File = require('vinyl');
 
 marked.Renderer.prototype.code = function(code, lang, escaped) {
   if (this.options.highlight) {
@@ -34,15 +37,12 @@ marked.setOptions({
     sanitize: false,
     smartLists: true,
     smartypants: true,
-    highlight: function (code, lang) {
-        return highlight(code, lang, 'html')
-    }
 });
 
 //=======================================================================
 //         NUNJUCKS
 //=======================================================================
-var template_path = ['design/templates'];
+var template_path = ['design/templates', 'content/_news/'];
 
 var manageEnvironment = function(environment) {
 
@@ -66,6 +66,10 @@ var manageEnvironment = function(environment) {
     environment.addFilter('date', dateFilter);
 }
 
+//=======================================================================
+//         CONTENT
+//=======================================================================
+
 gulp.task('pages', function() {
     return gulp.src(['content/**/*.j2', '!content/_*/**/*'])
         .pipe(debug({minimal: true}))
@@ -79,6 +83,125 @@ gulp.task('pages', function() {
         .pipe(gulp.dest('dist'))
         .pipe(browserSync.stream());
 });
+
+var processNews = function(file) {
+
+    return through.obj(function(file, enc, cb) {
+
+        var match_news = /(\d{4})-(\d{2})-(\d{2})-(.*)\/index.j2$/.exec(file.relative);
+
+        data = file.data || {};
+
+        if(!match_news) {
+            throw "wrong news entry name: " + file.relative;
+        }
+
+        var year  = match_news[1];
+        var month = match_news[2];
+        var day   = match_news[3];
+        var slag  = match_news[4];
+
+        data['post_file'] = file.relative;
+        data['date'] = new Date(year + "-" + month + "-" + day);
+        data['url'] = '/news/' + slag + '/';
+        data['permalink'] = 'https://www.gnuradio.org' + data.url;
+
+        file.stem = 'index';
+        file.path = file.base + data['url'] + 'index.html';
+        file.data = data;
+
+        this.push(file);
+        cb();
+    });
+};
+
+var prepareNewsEntry = function() {
+
+    return through.obj(function bufferContents(file, enc, cb) {
+        file.contents = fs.readFileSync('design/templates/news_entry.j2'),
+        this.push(file);
+        cb();
+    });
+};
+
+gulp.task('news:assets', function() {
+    // todo: pictures of news entries should be in corresponding folder
+    // ...and not in assets
+});
+
+// static content
+gulp.task('news:pages', function() {
+    return gulp.src(['content/_news/*/index.j2'])
+        .pipe(debug({minimal: true}))
+        .pipe(frontMatter({
+            property: 'data',
+            remove: true
+        }))
+        .pipe(processNews())
+        .pipe(prepareNewsEntry())
+        .pipe(nunjucksRender({
+            path: template_path,
+            envOptions: {
+                autoescape: false
+            },
+            manageEnv: manageEnvironment
+        }))
+        .pipe(gulp.dest('dist'))
+        .pipe(browserSync.stream());
+});
+
+var prepareNewsIndex = function() {
+
+    var posts = [];
+
+    function bufferContents(file, enc, cb) {
+        var p = {
+            url: data.url,
+            date: data.date,
+            template: data.post_file
+        };
+        posts.push(p);
+        cb();
+    };
+
+    function endStream(cb) {
+        posts.sort(function(a, b) {
+            if(a.date < b.date) return 1;
+            if(a.date > b.date) return -1;
+            return 0;
+        });
+        var f = new File({
+            cwd: '',
+            base: null,
+            path: 'news/index.html',
+            contents: fs.readFileSync('design/templates/news.j2')
+        });
+        f.data = {}
+        f.data.news = posts;
+
+        this.push(f);
+        cb();
+    };
+
+    return through.obj(bufferContents, endStream);
+};
+
+gulp.task('news:index', function() {
+    return gulp.src('content/_news/*/index.j2')
+        .pipe(processNews())
+        .pipe(prepareNewsIndex())
+        .pipe(nunjucksRender({
+            path: template_path,
+            envOptions: {
+                autoescape: false
+            },
+            manageEnv: manageEnvironment
+        }))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('news', ['news:pages', 'news:assets', 'news:index']);
+
 
 // static content
 gulp.task('assets', function() {
@@ -103,7 +226,7 @@ gulp.task('css', function() {
 //=======================================================================
 //         BUILD / RUN
 //=======================================================================
-gulp.task('default', ['pages', 'assets', 'css']);
+gulp.task('default', ['pages', 'assets', 'css', 'news']);
 
 gulp.task('clean', function() {
     del(['dist']);
